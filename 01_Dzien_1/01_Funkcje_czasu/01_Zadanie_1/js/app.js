aeda933cbb40aa34fef8333334b9dad3415f943b
kamil079 <kamil.szczepaniec@wp.pl>
2022-01-31 21:10:49 +0100

    test test

--- 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
@@ -1,1 +1,8 @@
 
+let timeOut = setTimeout(() => {
+    console.log('Wygenerowano z setInterval')
+}, 4000);
+
+let interval = setInterval(() => {
+    console.log('JS Rules')
+}, 10000);
--- 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
@@ -1,1 +1,20 @@
 
+function countHello(n) {
+    let counter = 1;
+    for (let i = 0; i < n; i++) {
+        let interval = setInterval(() => {
+            if (i === n) {
+                clearInterval(interval);
+            } else {
+                console.log('hello ' + counter);
+            }
+            counter++;
+        }, n * 1000);
+        // counter++;
+
+    }
+}
+
+
+let n = 4;
+countHello(n);
