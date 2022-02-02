658fb47226f7117bc53ef9108f19ebc1202d30e4
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-02 21:41:22 +0100

    working on arrays

--- 01_Dzien_1/03_Tablice/04_Zadanie_4/js/app.js
+++ 01_Dzien_1/03_Tablice/04_Zadanie_4/js/app.js
@@ -1,1 +1,5 @@
+let printTable = function(array) {
+    array.forEach(el => console.log(el))};
 
+let array = [1,2,3,4,5];
+printTable(array);
--- 01_Dzien_1/03_Tablice/05_Zadanie_5/js/app.js
+++ 01_Dzien_1/03_Tablice/05_Zadanie_5/js/app.js
@@ -1,1 +1,6 @@
+let multiply = function(array) {
+   return array.reduce((p,c) => p * c);
+}
 
+let array = [1,2,3,4,5,6,7]
+console.log(multiply(array));
