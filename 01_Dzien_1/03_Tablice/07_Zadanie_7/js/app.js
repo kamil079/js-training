45a4f3105be32dc15dcca71e1775c173473210fb
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-03 23:50:11 +0100

    more arrays

--- 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
+++ 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
@@ -1,1 +1,8 @@
+let sortArr = function(arr) {
+    arr.sort((a, b) => a - b)
+    return arr;
+}
 
+
+let arr = [2,3,42,54,23,1,4,554,6556,35,435,2342,42,654,56]
+console.log(sortArr(arr));
--- 01_Dzien_1/03_Tablice/08_Zadanie_8/js/app.js
+++ 01_Dzien_1/03_Tablice/08_Zadanie_8/js/app.js
@@ -1,1 +1,29 @@
+let tab1 = [8,3,22,5];
+let tab2 = [1,3,2];
 
+function addArrays(tab1,tab2){
+    let newTab = [];
+    let short;
+    let long;
+
+    if (tab1.length < tab2.length){
+        short = tab1;
+        long = tab2;
+    }else {
+        short = tab2;
+        long = tab1;
+    }
+
+    for (let i = 0; i < short.length; i++){
+        newTab.push(short[i] + long[i]);
+
+    }
+    for (let j = short.length; j < long.length; j++){
+        newTab.push(long[j]);
+
+    }
+    return newTab;
+}
+
+
+console.log(addArrays(tab2,tab1));
