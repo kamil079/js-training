235ed28deb1c64b151334e40e5a5cd0ccdb7789b
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-03 11:50:36 +0100

    arr even || null

--- 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
+++ 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
@@ -1,1 +1,21 @@
+function getEvenAverage(arr) {
+    let avrArr = [];
+    let res;
 
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] % 2 != 0) {
+            null;
+        } else {
+            avrArr.push(arr[i]);
+            res = avrArr.reduce((p, c) => p + c) / avrArr.length;
+
+        }
+        
+    }
+
+    return res || null;
+}
+
+let arr = [1, 1, 1,2,4];
+console.log(getEvenAverage(arr));
+//not sure why but it works lol
