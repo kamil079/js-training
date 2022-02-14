66bb670098ceda6b57f190b6b5984f9f585f1fa4
Kamil Szczepaniec <k.szczepaniec@astaldi.local>
2022-02-14 10:54:02 +0100

    adding new position to list

---
+++ 07_Dzien_4/01_Tworzenie_elementow/04_Zadanie_4/js/app.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector('.btn');
+let nrValue = document.querySelector('#orderId');
+let itemValue = document.querySelector('#item');
+let nrOfItemsValue = document.querySelector('#quantity');
+let tBody = document.querySelector('tbody');
+
+btn.addEventListener('click', adding);
+
+function adding() {
+    let newTr = document.createElement('tr');
+    let nrTd = document.createElement('td');
+    let itemTd = document.createElement('td');
+    let quantityTd = document.createElement('td');
+    
+    tBody.appendChild(newTr);
+
+    newTr.appendChild(nrTd);
+    nrTd.innerText = nrValue.value;
+
+    newTr.appendChild(itemTd);
+    itemTd.innerText = itemValue.value;
+    
+    newTr.appendChild(quantityTd);
+    quantityTd.innerText = nrOfItemsValue.value;
+}
