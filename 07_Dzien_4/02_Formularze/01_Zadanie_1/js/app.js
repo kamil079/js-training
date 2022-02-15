47cb45932c65475c91fc96f15dce8cf3a33fc589
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-16 00:32:15 +0100

    first form

--- 07_Dzien_4/02_Formularze/01_Zadanie_1/js/app.js
+++ 07_Dzien_4/02_Formularze/01_Zadanie_1/js/app.js
@@ -1,20 +1,38 @@
-let emailEl = document.querySelector('#email');
-let nameEl = document.querySelector('#name');
-let surnameEl = document.querySelector('#surname');
-let pass1El = document.querySelector('#pass1');
-let pass2El = document.querySelector('#pass2');
-let checkbox = document.querySelector('#agree');
+let emailEl = document.querySelector("#email");
+let nameEl = document.querySelector("#name");
+let surnameEl = document.querySelector("#surname");
+let pass1El = document.querySelector("#pass1");
+let pass2El = document.querySelector("#pass2");
+let checkbox = document.querySelector("#agree");
 
-let errMsg = document.querySelector('#error-message');
-let sucMsg = document.querySelector('#success-message');
+let errMsg = document.querySelector("#error-message");
+let sucMsg = document.querySelector("#success-message");
 
-let formEl = document.querySelector('form');
+let formEl = document.querySelector("form");
 
-formEl.addEventListener('submit', function(e) {
+formEl.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    if(!emailEl.value.includes('@')) {
-        errMsg.classList.remove('d-none');
-        errMsg.innerText = 'Email musi posiadać znak @';
+    if (!emailEl.value.includes("@")) {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Email musi posiadać znak @";
+    } else if (nameEl.value.length && surnameEl.value.length <= 2) {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Twoje imię lub nazwisko jest za krótkie";
+    } else if (pass1El.value !== pass2El.value) {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Hasła nie są takie same lub puste";
+    } else if (pass1El.value === "") {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Hasła nie są takie same lub puste";
+    } else if (pass2El.value === "") {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Hasła nie są takie same lub puste";
+    } else if (!checkbox.checked) {
+        errMsg.classList.remove("d-none");
+        errMsg.innerText = "Musisz zaakceptować warunki";
+    } else {
+        sucMsg.classList.remove("d-none");
+        sucMsg.innerText = "Formularz wysłany";
     }
-})
+});
