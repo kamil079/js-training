a69b9f011b6763f495738b7d963538fc0fdf7f5d
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-18 22:34:03 +0100

    to be cd

--- 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
+++ 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
@@ -8,10 +8,26 @@ let masterPic = document.querySelector("#mastercard-icon");
 allCards.forEach((el) => (el.style.visibility = "hidden"));
 
 inputCardNr.addEventListener("keyup", verifyVisa);
+inputCardNr.addEventListener("keyup", verifyAmPic);
+inputCardNr.addEventListener("keyup", verifymasterPic);
 
 function verifyVisa() {
-    if (inputCardNr.value[0] == 1) {
+    if (inputCardNr.value[0] == 4) {
         visaPic.style.visibility = "visible"; //why this is not workin..
         console.log(visaPic);
+    }
+}
+
+function verifyAmPic() {
+    if (inputCardNr.value[0] == 6) {
+        amPic.style.visibility = "visible"; //why this is not workin..
+        console.log(amPic);
+    }
+}
+
+function verifymasterPic() {
+    if (inputCardNr.value[0] == 7) {
+        masterPic.style.visibility = "visible"; //why this is not workin..
+        console.log(masterPic);
     }
 }
