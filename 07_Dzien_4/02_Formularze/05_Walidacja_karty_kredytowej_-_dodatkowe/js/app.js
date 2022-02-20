4ffce5179bdde16679c9d2618d6d34c308e65d81
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-20 21:35:58 +0100

    cd

--- 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
+++ 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
@@ -1,40 +1,38 @@
 let inputCardNr = document.querySelector("#cardNumber");
-let cardType = document.querySelector("#type");
 
 let allCards = document.querySelectorAll("i");
-let visaPic = document.querySelector("#visa-icon");
-let amPic = document.querySelector("#amex-icon");
-let masterPic = document.querySelector("#mastercard-icon");
+let iVisa = document.querySelector("i, #visa-icon");
+let iAmex = document.querySelector("i, #amex-icon");
+let iMaster = document.querySelector("i, #mastercard-icon");
 
 allCards.forEach((el) => (el.style.visibility = "hidden"));
 
 inputCardNr.addEventListener("keyup", verifyVisa);
-inputCardNr.addEventListener("keyup", verifyAmPic);
-inputCardNr.addEventListener("keyup", verifymasterPic);
+// inputCardNr.addEventListener("keyup", verifyAmPic);
+// inputCardNr.addEventListener("keyup", verifymasterPic);
 
-function verifyVisa(e) {
-    cardType.innerText = '';
-    switch (e.target.value[0]) {
+function verifyVisa() {
+    switch (this.value[0]) {
         case "4":
-            if (e.target.value.length >= 13 && e.target.value.length <= 16) {
+            if (this.value.length >= 13 && this.value.length <= 16) {
+                iVisa.style.visibility = "visible";
+                console.log(this.value)
             }
+            break;  
     }
-    visaPic.style.visibility = "visible"; //why this is not workin..
-    cardType.innerText = 'visa'
-    
-    // console.log(e.target.value[0]);
+    // allCards[0].style.visibility = "visible";
 }
 
-function verifyAmPic() {
-    if (inputCardNr.value[0] == 6) {
-        amPic.style.visibility = "visible"; //why this is not workin..
-        console.log(amPic);
-    }
-}
+// function verifyAmPic() {
+//     if (inputCardNr.value[0] == 6) {
+//         amPic.style.visibility = "visible"; //why this is not workin..
+//         console.log(amPic);
+//     }
+// }
 
-function verifymasterPic() {
-    if (inputCardNr.value[0] == 7) {
-        masterPic.style.visibility = "visible"; //why this is not workin..
-        console.log(masterPic);
-    }
-}
+// function verifymasterPic() {
+//     if (inputCardNr.value[0] == 7) {
+//         masterPic.style.visibility = "visible"; //why this is not workin..
+//         console.log(masterPic);
+//     }
+// }
