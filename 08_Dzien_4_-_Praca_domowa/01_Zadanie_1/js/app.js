8b52252212b226e71caa89c706a6b5aeb3c16db6
kamil079 <kamil.szczepaniec@wp.pl>
2022-02-21 19:53:07 +0100

    done form

--- 08_Dzien_4_-_Praca_domowa/01_Zadanie_1/js/app.js
+++ 08_Dzien_4_-_Praca_domowa/01_Zadanie_1/js/app.js
@@ -1,48 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-  form();
-  //   pizzaOrder();
+    form();
+    pizzaOrder();
 });
 let sum = 0;
 
 let form = function () {
-  document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let orderSum = document.querySelector(".order-info");
+        let elToSum = document.createElement("span");
+
+        orderSum.innerText = `Do zapłaty: ${sum}`;
+
+        orderSum.append(elToSum);
+    });
+};
+
+let pizzaOrder = function () {
+    let checkboxes = document.querySelectorAll("[type=checkbox]");
+    let btns = document.querySelectorAll(".btn");
+    let addAll = btns[0];
+    let rmvAll = btns[1];
+
+    addAll.addEventListener("click", () => {
+        checkboxes.forEach((el) => (el.checked = true));
+    });
+
+    rmvAll.addEventListener("click", () => {
+        checkboxes.forEach((el) => (el.checked = false));
+    });
+
+    checkboxes.forEach((el) => el.addEventListener("click", calc));
+};
+
+let calc = () => {
+    let selected = document.querySelectorAll("[type=checkbox]:checked");
+
+    selected.forEach((el) => (sum += parseFloat(el.dataset.price)));
 
     let orderSum = document.querySelector(".order-info");
     let elToSum = document.createElement("span");
 
-    orderSum.innerText = `Do zapłaty: ${sum}`;
+    orderSum.innerText = `Do zapłaty: ${sum + 35} zł`;
 
     orderSum.append(elToSum);
-  });
-
-  let checkboxes = document.querySelectorAll("[type=checkbox]");
-  let btns = document.querySelectorAll(".btn");
-  let addAll = btns[0];
-  let rmvAll = btns[1];
-
-  addAll.addEventListener("click", () => {
-    checkboxes.forEach((el) => (el.checked = true));
-  });
-
-  rmvAll.addEventListener("click", () => {
-    checkboxes.forEach((el) => (el.checked = false));
-  });
-
-  checkboxes.forEach((el) => el.addEventListener("click", calc));
 };
-
-let calc = () => {
-  let selected = document.querySelectorAll("[type=checkbox]:checked");
-
-  selected.forEach((el) => (sum += parseFloat(el.dataset.price)));
-  console.log(sum);
-
-  let orderSum = document.querySelector(".order-info");
-  let elToSum = document.createElement("span");
-
-  orderSum.innerText = `Do zapłaty: ${sum + 35} zł`;
-
-  orderSum.append(elToSum);
-};
-//to co w calc dodac do dodawania i usuwania i dziala ale sprawdz 
+//to co w calc dodac do dodawania i usuwania i dziala ale sprawdz
