c2ae6a4ab451a897b5198fd7b1f104417f8b1544
Kamil Szczepaniec <kamil.szczepaniec@wp.pl>
2022-02-21 12:23:58 +0100

    card validator

--- 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
+++ 07_Dzien_4/02_Formularze/05_Walidacja_karty_kredytowej_-_dodatkowe/js/app.js
@@ -1,34 +1,51 @@
 let inputCardNr = document.querySelector("#cardNumber");
 
+inputCardNr.addEventListener("keyup", verifyCard);
+
 let allCards = document.querySelectorAll("i");
-let iVisa = document.querySelector("i, #visa-icon");
-let iAmex = document.querySelector("i, #amex-icon");
-let iMaster = document.querySelector("i, #mastercard-icon");
 
 allCards.forEach((el) => (el.style.visibility = "hidden"));
 
-inputCardNr.addEventListener("keyup", verifyVisa);
-// inputCardNr.addEventListener("keyup", verifyAmPic);
-// inputCardNr.addEventListener("keyup", verifymasterPic);
+function verifyCard(e) {
+  let iVisa = document.querySelector("i, #visa-icon");
+  let iAmex = document.querySelector("i, #amex-icon");
+  let iMaster = document.querySelector("i, #mastercard-icon");
 
-function verifyVisa() {
-    switch (this.value[0]) {
-        case "4":
-            if (this.value.length >= 13 && this.value.length <= 16) {
-                iVisa.style.visibility = "visible";
-                console.log(this.value)
-            }
-            break;  
-    }
-    // allCards[0].style.visibility = "visible";
+  iVisa.style.visibility = "hidden";
+  iAmex.style.visibility = "hidden";
+  iMaster.style.visibility = "hidden";
+
+  e.target.style.borderColor = "#ccc";
+
+  switch (this.value[0]) {
+    case "4":
+      iVisa.style.visibility = "visible";
+      if (e.target.value.length >= 13 && e.target.value.length <= 16) {
+        e.target.style.borderColor = "green";
+      }
+      break;
+    case "5":
+      iAmex.style.visibility = "visible";
+      if (e.target.value.length === 16) {
+        e.target.style.borderColor = "green";
+      }
+      break;
+    case "3":
+      iMaster.style.visibility = "visible";
+      if (
+        (e.target.value.length === 15 && e.target.value[1] === '4') || e.target.value[1] === '7') {
+        e.target.style.borderColor = "green";
+      }
+      break;
+  }
 }
 
-// function verifyAmPic() {
-//     if (inputCardNr.value[0] == 6) {
-//         amPic.style.visibility = "visible"; //why this is not workin..
-//         console.log(amPic);
-//     }
-// }
+function verifyAmPic() {
+  if (inputCardNr.value[0] == 6) {
+    amPic.style.visibility = "visible"; //why this is not workin..
+    console.log(amPic);
+  }
+}
 
 // function verifymasterPic() {
 //     if (inputCardNr.value[0] == 7) {
